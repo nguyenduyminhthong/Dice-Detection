b23c08d62f1ffb050190710ad29604f69448392f
nguyenduyminhthong <nguyenduyminhthong@gmail.com>
2021-07-05 02:26:34 +0700

    Clean some temporary variables

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -16,17 +16,8 @@ import org.opencv.imgproc.Imgproc;
 
 public class Main {
     private static final Scalar COLOR = new Scalar(255, 255, 0, 0);
-    private static final int FONT = Imgproc.FONT_HERSHEY_SIMPLEX;
-    private static final int SCALE = 1;
-    private static final int THICKNESS = 3;
-    private static final int DICE_AREA = 400;
-    private static final int DOT_AREA = 120;
-    private static final int THRESH = 158;
-    private static final int MAX_VALUE = 255;
-
-    private static int number_of_dots, child_level;
-    private static RotatedRect rect;
-    private static double area;
+    private static final int FONT = Imgproc.FONT_HERSHEY_SIMPLEX, SCALE = 1, THICKNESS = 3,
+            DICE_AREA = 400, DOT_AREA = 120, THRESH = 158, MAX_VALUE = 255;
 
     static {
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
@@ -34,47 +25,43 @@ public class Main {
 
     public static void main(String... args) {
         Mat raw_img = Imgcodecs.imread("src/main/resources/images/Dice.jpg");
-        Mat gray_img = raw_img.clone();
+        Mat gray_img = raw_img.clone(), blurred_img = raw_img.clone(), binary_img = raw_img.clone(),
+                hierarchy = new Mat();
+
         Imgproc.cvtColor(raw_img, gray_img, Imgproc.COLOR_BGR2GRAY);
-
-        Mat blurred_img = raw_img.clone();
         Imgproc.GaussianBlur(gray_img, blurred_img, new Size(5, 5), 0);
-
-        Mat binary_img = raw_img.clone();
         Imgproc.threshold(blurred_img, binary_img, THRESH, MAX_VALUE, Imgproc.THRESH_BINARY);
 
         List<MatOfPoint> contours = new ArrayList<MatOfPoint>();
-        Mat hierarchy = new Mat();
         Imgproc.findContours(binary_img, contours, hierarchy, Imgproc.RETR_TREE,
                 Imgproc.CHAIN_APPROX_SIMPLE);
 
         for (int i = 0; i < contours.size(); ++i) {
             if (hierarchy.get(0, i)[2] != -1) {
-                rect = Imgproc.minAreaRect(new MatOfPoint2f(contours.get(i).toArray()));
-                area = rect.boundingRect().area();
+                RotatedRect rect = Imgproc.minAreaRect(new MatOfPoint2f(contours.get(i).toArray()));
+                double area = rect.boundingRect().area();
                 if (area > DICE_AREA) {
-                    number_of_dots = 0;
-                    child_level = (int) hierarchy.get(0, i)[2];
+                    int number_of_dots = 0, child_level = (int) hierarchy.get(0, i)[2];
                     while (child_level != -1) {
                         area = Imgproc
                                 .minAreaRect(new MatOfPoint2f(contours.get(child_level).toArray()))
                                 .boundingRect().area();
                         if (area > DOT_AREA)
-                            number_of_dots += 1;
+                            ++number_of_dots;
                         child_level = (int) hierarchy.get(0, child_level)[0];
                         Point points[] = new Point[4];
                         rect.points(points);
-                        for (int j = 0; j < 4; ++j)
-                            Imgproc.line(raw_img, points[j], points[(j + 1) % 4], COLOR, THICKNESS,
+                        for (int j = 0; j < 4;)
+                            Imgproc.line(raw_img, points[j], points[++j % 4], COLOR, THICKNESS,
                                     Imgproc.LINE_AA);
                     }
-                    Imgproc.putText(raw_img, Integer.toString(number_of_dots), rect.center, FONT,
-                            SCALE, COLOR, THICKNESS);
+                    Imgproc.putText(raw_img, number_of_dots + "", rect.center, FONT, SCALE, COLOR,
+                            THICKNESS);
                 }
             }
         }
 
-        HighGui.imshow("Window", raw_img);
+        HighGui.imshow("Output", raw_img);
         HighGui.waitKey();
         System.exit(0);
     }
